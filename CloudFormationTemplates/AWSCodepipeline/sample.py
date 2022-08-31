c2819c78c859e904f5284e7a2c9cd2859bfd8dba
saikrishnna29 <panugantisaikrishna@icloud.com>
2022-08-31 20:38:23 +0530

    codepipeline

---
+++ CloudFormationTemplates/AWSCodepipeline/sample.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+directory = "pyscripts"
+
+path = os.getcwd()
+
+location = os.path.join(path, directory)
+
+created_dir = os.mkdir(location)
+
+print(f"The {directory} is available at {location}")
